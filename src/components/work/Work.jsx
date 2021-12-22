763b1e9036f4545997320bef33eb4fcf15ffdfb5
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-23 00:00:06 +0900

    work display flex

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -3,13 +3,14 @@ import "./work.css"
 const Work = () => {
     return (
         <div className="w">
+            {/* memopad effect */}
             <div className="w-browser">
                 <div className="w-circle"></div>
                 <div className="w-circle"></div>
                 <div className="w-circle"></div>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default Work
--- src/components/workList/WorkList.jsx
+++ src/components/workList/WorkList.jsx
@@ -6,11 +6,15 @@ const WorkList = () => {
         <div className="wl">
             <div className="wl-text">
                 <h1 className="wl-title">Work Catalogue</h1>
-                    <p className="wl-desc">
-                        I have worked on many projects. etc etc.
-                    </p>
+                <p className="wl-desc">
+                    I have worked on many projects. etc etc.
+                </p>
             </div>
+
             <div className="wl-list">
+                <Work />
+                <Work />
+                <Work />
                 <Work />
                 <Work />
                 <Work />
