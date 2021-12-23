cf9272fec23f9c1485cd427b247820362715b1dd
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-24 00:29:55 +0900

    jsmail - contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -17,28 +17,37 @@ const Contact = () => {
                                 alt="" 
                                 className="c-icon" 
                             /> */}
-                            <i className="fas fa-phone-square-alt"></i>
+                            <i className="c-icon fas fa-phone-square-alt fa-2x"></i>
                             +82 01-8215-8540
                         </div>
                         <div className="c-info-item">
-                            <i className="fas fa-envelope"></i>
+                            <i className="c-icon fas fa-envelope fa-2x"></i>
                             {/* <img src={Email} alt="" className="c-icon"/> */}
                             starmireu@gmail.com
                         </div>
                         <div className="c-info-item">
-                            <i class="fab fa-linkedin"></i>
+                            <i class="c-icon fab fa-linkedin fa-2x"></i>
                             {/* <img src={LinkedIn} alt="" className="c-icon"/> */}
                             https://www.linkedin.com/in/jongwan-kim-89500a194/
                         </div>
                         <div className="c-info-item">
-                            <i class="fab fa-github-square"></i>
+                            <i class="c-icon fab fa-github-square fa-2x"></i>
                             {/* <img src={Github} alt="" className="c-icon"/> */}
                             https://github.com/mireu-san
                         </div>
                     </div>
                 </div>
                 <div className="c-right">
-                    <h2></h2>
+                    <p className="c-desc">
+                        <b>test</b> test.
+                    </p>
+                    <form>
+                        <input type="text" placeholder="Name" name="user_name" />
+                        <input type="text" placeholder="Name" name="user_name" />
+                        <input type="text" placeholder="Name" name="user_name" />
+                        <textarea rows="5" placeholder="Message" name="message" />
+                        <button>Send</button>
+                    </form>
                 </div>
             </div>
         </div>
