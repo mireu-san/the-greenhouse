ee19276400179c10fbc5e78d8997f792277cd768
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-23 18:05:40 +0900

    checkpoint

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,8 @@
 import "./contact.css"
+import Phone from "../../img/phone.png"
+import Email from "../../img/email.png"
+
+fontawesome vs image?
 
 const Contact = () => {
     return (
@@ -6,7 +10,18 @@ const Contact = () => {
             <div className="c-bg"></div>
             <div className="c-wrapper">
                 <div className="c-left">
-                    <h1></h1>
+                    <h1 className="c-title">
+                        Contact
+                    </h1>
+                    <div className="d-info">
+                        <div className="c-info-item">
+                            <img 
+                                src="" 
+                                alt="" 
+                                className="c-icon" 
+                            />
+                        </div>
+                    </div>
                 </div>
                 <div className="c-right">
                     <h2></h2>
