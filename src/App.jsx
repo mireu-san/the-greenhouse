1bb703f4a1e326f5bfb5ac3de55c0cf9be5ceb6c
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-24 21:54:50 +0900

    looking good - background css on App.jsx as global

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
+import Toggle from "./components/toggle/Toggle";
 import WorkList from "./components/workList/WorkList";
 
 
 
 const App = () => {
+// useState method does work to distribute button on each pages, but not appropriate for large scale web
   return (
-    <div>
+    <div style={{backgroundColor: "#23252F", color: "white"}}>
+      <Toggle />
       <Intro />
       <About />
       <WorkList />
---
+++ src/components/toggle/Toggle.jsx
@@ -0,0 +1,14 @@
+import "./toggle.css"
+
+
+const Toggle = () => {
+    return (
+        <div className="t">
+            <i class="t-icon fas fa-sun"></i>
+            <i class="t-icon fas fa-moon"></i>
+            <div className="t-button"></div>
+        </div>
+    )
+}
+
+export default Toggle
