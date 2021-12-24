eaa53d77883ce35067f5988b8e7b240b26cae219
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-24 22:13:59 +0900

    1.5/2 toggle - functions OK

--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -1,12 +1,19 @@
+import { useContext } from "react"
+import { ThemeContext } from "../../context"
 import "./toggle.css"
 
 
 const Toggle = () => {
+    const theme = useContext(ThemeContext)
+
+    const handleClick = () => {
+        theme.dispatch({type:"TOGGLE"})
+    }
     return (
         <div className="t">
             <i class="t-icon fas fa-sun"></i>
             <i class="t-icon fas fa-moon"></i>
-            <div className="t-button"></div>
+            <div className="t-button" onClick={handleClick}></div>
         </div>
     )
 }
