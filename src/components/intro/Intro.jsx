430c5cc7fae901093b5b6b08122bfbde4065369a
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-25 20:58:15 +0900

    minor typo fixed - intro(profile) .i-left

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -21,63 +21,6 @@ const Intro = () => {
                         저는 웹 개발자로서 작업 중 발생하는 문제를 자율적으로 디버깅 해 냄으로서, 프론트엔드 개발자로서의 활동을 이어오고 있습니다. 유저가 불편함을 겪지 않는 웹 경험을 제공 하는 것에 초점을 두고 있습니다.
                     </p>
                 </div>
-                {/* mouse button */}
-                <svg
-                    width="75"
-                    height="75"
-                    viewBox="0 0 75 75"
-                    fill="none"
-                    stroke="black"
-                    className="i-scroll"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <g id="scroll">
-                        <path
-                        id="Vector"
-                        d="M40.5 15L34.5 9L28.5 15"
-                        stroke-width="3"
-                        stroke-miterlimit="10"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        />
-                        <path
-                        id="Vector_2"
-                        d="M28.5 24L34.5 30L40.5 24"
-                        stroke-width="3"
-                        stroke-miterlimit="10"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        />
-                        <g id="Group">
-                        <path
-                            id="Vector_3"
-                            d="M9 37.5H60"
-                            stroke-width="3"
-                            stroke-miterlimit="10"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                        </g>
-                        <path
-                        id="Vector_4"
-                        d="M34.5 27V9"
-                        stroke-width="2.9895"
-                        stroke-miterlimit="10"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        />
-                        <g id="Group_2">
-                        <path
-                            id="Vector_5"
-                            d="M9 27C9 12.918 20.418 1.5 34.5 1.5C48.5859 1.5 60 12.918 60 27C60 29.8906 60 45.1094 60 48C60 62.082 48.5859 73.5 34.5 73.5C20.418 73.5 9 62.082 9 48C9 45.1094 9 29.8906 9 27Z"
-                            stroke-width="3"
-                            stroke-miterlimit="10"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                    </g>
-                </g>
-                </svg>
             </div>
             
             
